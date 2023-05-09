38d17f8e28066dbd1fcf9604aa6fd78b279f31dc
ViVse <ivanisenko730@gmail.com>
2023-05-09 17:13:17 +0300

    moved productBadge into separate folder

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,6 +2,6 @@ export * from './Form/Form';
 export * from './Layout/Header/Header';
 export * from './Layout/Footer/Footer';
 export * from './UI/Gallery';
-export * from './Product/ProductBadge';
+export * from './Product/Badge/ProductBadge';
 export * from './Product/Offers/OffersTable';
 export * from './Product/Review/Review';
